d83ea1591638a70e64d290f28594493bd8619a97
Mauricio <maulberto3@hotmail.com>
2024-04-10 22:11:58 -0600

    first

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+
+// idea is to implement Open ES algo to solve n-dimensional problems
+
+// Pseudo code
+// fn main () {
+//     // TODO: use clap for command line arguments
+//     let num_dims = 99;
+//     let algo = Openes::new(num_dims, args...).unwrap();
+//     fn calc_fit() -> ...
+//     let evo = Evolver::new(algo, calc_fit, args...).unwrap();
+//     let optimum = evo.solve().unwrap();
+// }
+
+
+fn main() {
+    println!("Hello, world!");
+}
